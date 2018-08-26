217543d29d00294bd92291a1bacaf8b62094b472
Guy Lepage <guylepage3@gmail.com>
2018-08-26 18:00:50 -0400

    removed webpack #25
    Signed-off-by: Guy Lepage <guylepage3@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Header from './components/Header';
-import './styles/Galaxy.css';
+import './App.scss';
 
 class App extends Component {
   render() {
