328a173248d8ef79b1e88072579f9082a1df5b94
Guy Lepage <guylepage3@gmail.com>
2018-09-16 10:10:48 -0400

    added bold to all footer #37
    Signed-off-by: Guy Lepage <guylepage3@gmail.com>

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,21 +4,21 @@ class Footer extends Component {
   render() {
     return (
       <footer className="mt-auto mx-auto text-center">
-        <div className="mb-2 text-white-50 text-footer">
+        <div className="mb-2 text-white-50 text-footer font-weight-bold">
           <div className="mb-2">
-            <a href="https://github.com/galaxypi/galaxy" className="text-white font-weight-bold">
+            <a href="https://github.com/galaxypi/galaxy" className="text-white">
               GitHub
             </a>
             &nbsp;&nbsp;&middot;&nbsp;&nbsp;
-            <a href="https://github.com/orgs/galaxypi/people" className="text-white font-weight-bold">
+            <a href="https://github.com/orgs/galaxypi/people" className="text-white">
               Core team
             </a>
             &nbsp;&nbsp;&middot;&nbsp;&nbsp;
-            <a href="https://twitter.com/GalaxyPiLab" className="text-white font-weight-bold">
+            <a href="https://twitter.com/GalaxyPiLab" className="text-white">
               Twitter
             </a>
             &nbsp;&nbsp;&middot;&nbsp;&nbsp;
-            <a href="https://discord.gg/36K9nan" className="text-white font-weight-bold">
+            <a href="https://discord.gg/36K9nan" className="text-white">
               Chat (Discord)
             </a>
           </div>
